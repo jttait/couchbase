e6b8cc95cd421887026a536f55c51beeb690d1f3
Jonathon Tait <jttait@gmail.com>
2024-01-17 08:02:21 +0000

    couchbase image

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	}
 	defer cli.Close()
 
-	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+	reader, err := cli.ImagePull(ctx, "couchbase", types.ImagePullOptions{})
 	if err != nil {
 		panic(err)
 	}
